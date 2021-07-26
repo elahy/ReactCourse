498c2e439a58401c53f088e08d97df2e0ed76f64
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-07-26 19:46:41 +0600

    updated index.js

--- lifecycle/src/index.js
+++ lifecycle/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import Product from "./Component/Product";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
