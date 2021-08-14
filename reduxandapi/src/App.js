156ea65eeacebe947738f7862f2c0653d6de0b44
A.S.S.M.Q-E-Elahy <elahy777@gmail.com>
2021-08-14 21:23:20 +0600

    initiated project

---
+++ reduxandapi/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <main>Hello</main>
+      <footer>Bye</footer>
+    </div>
+  );
+}
+
+export default App;
